d6d5940302a1a71b58480b85329a822cabcfb611
Ben Cornelis & Drew <drew@finstrom.us>
2015-05-11 11:17:45 -0700

    let user login as HR or Project Manager

--- app.rb
+++ app.rb
@@ -6,9 +6,19 @@ require('./lib/sample')
 require('pry')
 require('pg')
 
+enable :sessions
 
 get('/test') do
   @test_var = 'Sinatra OK'
   erb(:test)
   #redirect to('/')
+end
+
+get('/') do
+  erb(:index)
+end
+
+post('/') do
+  session[:user_type] = params.fetch('user_type')
+  redirect to('/')
 end
--- spec/sample_integration_spec.rb
+++ spec/sample_integration_spec.rb
@@ -11,3 +11,13 @@ describe('Sinatra framework check', { :type => :feature }) do
     expect(page).to have_content('Sinatra OK')
   end
 end
+
+describe('Employee Tracer', { :type => :feature }) do
+  it('logs in as a class of user and saves as a sesion variable') do
+    visit('/')
+    choose('hr')
+    click_button('Continue')
+    expect(page).to(have_content('hr'))
+  end
+
+end
