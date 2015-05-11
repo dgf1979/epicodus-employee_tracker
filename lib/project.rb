896f1e48de84eeb86a94f0297a048e591c1cb28d
Ben Cornelis & Drew <drew@finstrom.us>
2015-05-11 14:37:47 -0700

    add remove employee method to project

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
   has_many(:employees)
+
+  def remove(employee_ids)
+    employee_ids.each { |employee_id| Employee.find(employee_id).update(:project_id => nil) }
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,4 +6,15 @@ describe(Project) do
     employee = Employee.create({ :name => 'Bob Loblaw', :project_id => project.id() })
     expect(project.employees()).to(eq([employee]))
   end
+
+  describe('#remove') do
+    it("removes employees from a project") do
+      project = Project.create({ :name => 'the Big Presentation'})
+      employee1 = Employee.create({ :name => 'Bob Loblaw', :project_id => project.id() })
+      employee2 = Employee.create({ :name => 'Fred', :project_id => project.id() })
+      employee3 = Employee.create({ :name => 'Cindy', :project_id => project.id() })
+      project.remove([employee2.id, employee3.id])
+      expect(project.employees).to(eq([employee1]))
+    end
+  end
 end
